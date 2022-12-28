acd8c45bbc413706a4db1ca99f52fde8c84260d2
Rajdeep-Das <rajdeepdas.india@gmail.com>
2022-12-28 15:51:17 +0530

    Basic Style Add

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,54 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import links from '../links.json';
+	let linksData = links;
+</script>
+
+<div class="main">
+	<h1 class="heading">Welcome to Link Chain</h1>
+	<div class="link_container">
+		{#each linksData as data}
+			<div class="linklist">
+				<a class="links" target="_blank" href={data.url} rel="noreferrer">
+					{data.name}
+				</a>
+			</div>
+		{/each}
+	</div>
+</div>
+
+<style>
+    .heading{
+        color: rgb(34 34 34 / 88%)
+    }
+	.link_container {
+		display: flex;
+		flex-direction: column;
+		gap: 8px;
+	}
+
+	.main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.linklist {
+		text-align: center;
+		padding: 4px;
+		min-height: 2rem;
+		min-width: 28rem;
+		background: linear-gradient(331deg, #edb8db 0%, #d6e1b4 100%);
+		border-radius: 2px;
+	}
+	.linklist:hover {
+		background: linear-gradient(133deg, #efa0d5 0%, #d4ea93 100%);
+	}
+	.links {
+		text-decoration: none;
+		font-size: 1.2rem;
+		font-weight: 700;
+		background: -webkit-linear-gradient(#636bdb, #a554db);
+		-webkit-background-clip: text;
+		background-clip: text;
+		-webkit-text-fill-color: transparent;
+	}
+</style>
